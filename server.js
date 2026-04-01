ff62822c4485483cdb190286f07c6806829c1784
Everton Oliveira <evertonsantosoliveira5@gmail.com>
2026-04-01 10:49:46 -0300

    fix database_url

--- server.js
+++ server.js
@@ -12,11 +12,7 @@ app.use(cors());
 
 // 🔥 CONEXÃO MYSQL (CORRIGIDA)
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  uri: process.env.DATABASE_URL,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
